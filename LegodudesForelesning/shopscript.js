2c1b39fdc3ff99dafbf638a885c20025864fb88d
NadiiaKuz <nadink1603@gmail.com>
2026-01-16 13:51:32 +0100

    forelesning 16.01

--- LegodudesForelesning/shopscript.js
+++ LegodudesForelesning/shopscript.js
@@ -19,10 +19,62 @@ function fetchProducts(){
 
 fetchProducts()
 
+//generer handlevogn
+function showCart(){
+    //unike produkter
+    let uniqueItems = new Set(cart) //streng med unike verdier
+    let uniqueArray = [...uniqueItems]
+    //oversikt over antal produkt
+    let cartItems = []
+    uniqueArray.map(item => {
+        cartItems.push({prodid: item, quantity: cart.filter(i => i === item).length})
+    })
+
+    //console.log(cartItems)
+
+    //gå gjennom cartItems for å lage HTML til handlevogn og regne ut totalpris
+    let cartHTML = ""
+    let totalPrice = 0
+    
+    cartItems.map(ci => {
+        //hente produktinformasjon
+        let product = products.find(i => i.prodid === ci.prodid)
+        //skrive ut HTML
+        cartHTML += `<tr>
+                        <td class="title">${product.title}</td>
+                        <td class="price">${product.price * ci.quantity}</td>
+                        <td class="quantity">${ci.quantity}</td>
+                        <td class="delete"><button onClick="deleteFromCart(${product.prodid})">X</button></td>
+                    </tr>`
+        //summere total pris
+        totalPrice += Number(product.price) * Number(ci.quantity)
+    })
+
+    if(cart.length === 0){
+        cartHTML += "<tr><td>Ingen varer i handlevognen enda</td></tr>"
+    }
+    //oppdaterer HTML ellement
+    document.getElementById("cart-items").innerHTML = cartHTML
+    document.getElementById("total-price").innerHTML = totalPrice
+    document.getElementById("cart-quantity").innerHTML = cart.length
+}
+
+//slette fra handlevogn
+function deleteFromCart(prodid){
+    let deleteIndex = cart.indexOf(prodid)
+    if(deleteIndex > -1) {
+        cart.splice(deleteIndex, 1)
+    }
+    //oppdatere handlevogn-utscrift
+    showCart()
+}
+
 //Legg til i handlevogn
 function addToCart(prodid) {
     console.log("Legg til produkt med id: " + prodid)
     cart.push(prodid)
     console.log(cart)
-    document.getElementById("cart-quantity").innerHTML = cart.length
+
+    //oppdater handlevognvisning
+    showCart()
 }
